f1082e77e487be596aafe9a0bb47b7fc37199af0
Josh Jiang <josh.jiang@ef.cn>
2025-07-04 17:34:27 +0800

    feat: some text

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,7 +4,7 @@ const Home: React.FC = () => {
   return (
     <div className="page">
       <h1>首頁</h1>
-      <p>歡迎來到我們的應用！</p>
+      <p>魔法世界的大门已经打开</p>
       <div className="content">
         <h2>功能特色</h2>
         <ul>
